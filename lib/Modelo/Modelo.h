75efabb4c9f17b5d5dc8ad0dc53d45523144029c
Pablo Almeida <pabloalmeida49@hotmail.com>
2024-07-29 19:34:30 -0300

    Ajuste no arquivo Modelo.h conforme ultimo commit

--- lib/Modelo/Modelo.h
+++ lib/Modelo/Modelo.h
@@ -21,7 +21,6 @@ private:
 public:
     Modelo();
     Modelo(string nome);
-    Modelo(string nome);
     Modelo(string nome, int tempoInicial, int tempoFinal);
 
     void setNome(const string &nome);
